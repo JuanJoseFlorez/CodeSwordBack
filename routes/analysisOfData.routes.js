bcae3d0e5e3e5dcbf90dc48caf117ad65565b70a
JuanJ <juanjofr2002@gmail.com>
2022-07-29 21:08:06 -0500

    Test

--- routes/analysisOfData.routes.js
+++ routes/analysisOfData.routes.js
@@ -4,7 +4,7 @@ const analysisOfData = require('../controllers/analysisOfData.controller');
 const middlewareLogin = require('../middlewares/middlewareLogin');
 
 //Ruta para traer publicaciones y el promedio de sus emociones
-router.get('/getAnalysisPublications', middlewareLogin.middleLoginAdmin, analysisOfData.getAnalysisPublications);
+router.get('/getAnalysisPublications', analysisOfData.getAnalysisPublications);
  
 //Exportar rutas a la App
 module.exports = router;
